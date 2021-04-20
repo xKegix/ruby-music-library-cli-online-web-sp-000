1110d2481d1787532709d5572e8a9ab1f4d3c083
Keginold Philip <keginoldp@hotmail.com>
2021-04-20 12:21:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,44 +1,65 @@
-require 'pry'
-
-	extend Findable # Take all methods in Findable, add them as class methods.
-	extend Persistable::ClassMethods # extend class methods.
-	include Persistable::InstanceMethods # include for instance methods.
-
 class Song
-	attr_accessor :name
+  attr_accessor :name
+  attr_reader :artist, :genre
+  @@all=[]
 
-	@@all = []
+  def initialize(name, artist=nil, genre=nil)
+    @name=name
+    self.artist=(artist) if artist!=nil
+    self.genre=(genre) if genre!=nil
+  end
 
-	def initialize
-		save
-	end
+  def self.all
+    @@all
+  end
 
-	def self.all
-		@@all
-	end
+  def self.destroy_all
+    self.all.clear
+  end
 
-	def save
-		@@all << self
-	end
+  def save
+    @@all << self
+  end
 
-	def self.reset_all
-		@@all.clear
-	end
+  def self.create(name)
+    Song.new(name).tap  {|song| song.save}
+  end
 
-	def self.count
-		@@all.size
-	end
+  def artist=(artist)
+    @artist=artist
+    artist.add_song(self)
+  end
 
+  def genre=(genre)
+    @genre=genre
+    genre.add_song(self)
+  end
 
+  def self.find_by_name(name)
+    self.all.detect {|song| song.name==name}
+  end
 
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? song=self.find_by_name(name) : song=self.create(name)
+  end
 
-end
+  def self.new_from_filename(filename)
+    parse = filename.split(" - ")
+    parse[2].chomp!(".mp3")
+    if !self.find_by_name(parse[1])
+      song = Song.new(parse[1])
+      song.artist=Artist.find_or_create_by_name(parse[0])
+      song.genre=Genre.find_or_create_by_name(parse[2])
+    else
+      song=self.find_by_name(parse[1])
+      song.artist=Artist.find_or_create_by_name(parse[0])
+      song.genre=Genre.find_or_create_by_name(parse[2])
+    end
+    song
+  end
 
-
-
-
-
-
-
-
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+  end
 end
