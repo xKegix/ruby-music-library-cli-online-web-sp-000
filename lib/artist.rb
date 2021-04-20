c5b289f3a1edeb2e0b538aa046f1384046d63ce2
Keginold Philip <keginoldp@hotmail.com>
2021-04-20 12:18:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,78 +1,45 @@
 require 'pry'
+#require_relative './song'
 
-class Artist #self
-	extend Findable
-	attr_accessor :name, :songs
+class Artist
+  extend Concerns::Findable
+  attr_accessor :name, :genre
+  @@all=[]
 
-	@@all = [] 			# keeps track of all Artist
+  def initialize(name)
+    @name=name
+  end
 
-	def initialize(name)
-		@name = name
-		@songs =[] # track of songs
-	end
+  def self.all
+    @@all
+  end
 
-	def self.all 	# @@all accessible through this class method.
-		@all
-	end
+  def self.destroy_all
+    self.all.clear
+  end
 
-	# def save 	# adds instance itself into @@all class variable. To store its data.
-	# 	@@all << self
-	# end
-	#
-	# def self.destroy_all  # empty @@all array.
-	# 	@@all.clear
-	# end
-	#
-	# def self.count
-	# 	@@all.size
-	# end
+  def save
+    @@all << self
+  end
 
-	def self.create(name) 		# custom constructor, instantiates an instance also saves, forcing to exist immediatrly.
-		artist = self.new(name) #create new artist.
-		artist.save 						# save that artist.
-		artist 									# show the artist that is made.
-	end
+  def self.create(name)
+    Artist.new(name).tap {|artist| artist.save}
+  end
 
-	def add_song(song) # only add songs not in array already.
-		song.artist = self if song.artist == nil # this is songs artist if given artist is not there.
-		@songs << song if self.songs.include?(song) == false # then add song to @song array if songs of artist is not included in @song array.
-	end
+  def songs
+    @songs=[]
+    Song.all.each {|song| @songs << song if song.artist==self && !@songs.include?(song)}
+    @songs
+  end
 
-	def songs # show all songs.
-		@songs
-	end
+  def genres
+    genre_array=[]
+    self.songs.each {|song| genre_array << song.genre if !genre_array.include?(song.genre)}
+    genre_array
+  end
 
-	def genres
-		@songs.collect {|song| song.genre}.uniq
-	end
-
-	# def self.find_by_name(name)
-	# 	@@all.detect {|artist| artist.name == name} # return first artist that makes the statement true - name of every artist to the name passed into this method.
-	# end
-
-
-
-
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  def add_song(song)
+    song.artist=self if song.artist != self
+    @songs << song if !self.songs.include?(song)
+  end
 end
