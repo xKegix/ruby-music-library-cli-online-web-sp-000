7da920b08c12dc87bb3a6eb3c99c31cad97f65bc
Keginold Philip <keginoldp@hotmail.com>
2021-04-20 12:18:46 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,11 @@
- module Findable
+module Concerns
+  module Findable
+    def find_by_name(name)
+      self.all.detect {|x| x.name==name}
+    end
 
-		 def find_by_name(name)
-	 		self.all.detect {|artist| artist.name == name} # return first artist that makes the statement true - name of every artist to the name passed into this method.
-	 	end
-
-	 def find_or_create_by_name(name)
-		 find_by_name(name) || self.create(name)
-	 end
-
- end
+    def find_or_create_by_name(name)
+      self.find_by_name(name) ? x = self.find_by_name(name) : x = self.create(name)
+    end
+  end
+end
